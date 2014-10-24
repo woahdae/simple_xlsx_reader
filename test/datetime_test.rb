ee164a2819a5770a0bec0410831f51762bab0f6a
Valeriy Utyaganov <mac-1@Mac-mini-mac-1.local>
2014-10-24 17:02:42 +0400

    With test for small time value

--- test/datetime_test.rb
+++ test/datetime_test.rb
@@ -11,7 +11,8 @@ describe SimpleXlsxReader do
       "Datetimes" =>
         [[Time.parse("2013-08-19 18:29:59 UTC")],
          [Time.parse("2013-08-19 18:30:00 UTC")],
-         [Time.parse("2013-08-19 18:30:01 UTC")]]
+         [Time.parse("2013-08-19 18:30:01 UTC")],
+         [Time.parse("1899-12-30 00:30:00 UTC")]]
     })
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'pry'
+require 'time'
 
 $:.unshift File.expand_path("lib")
 require 'simple_xlsx_reader'
